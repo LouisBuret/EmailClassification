ae07e459f6c2f3df639c1ba8cf112c228aa59c56
LouisBURET <louis@buret.be>
2025-04-17 16:24:45 +0200

    flask works

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 from flask import Flask, render_template, request
+import keras
+from tensorflow import keras
+from keras.saving import load_model
+import spacy
+import pandas as pd
+
+nlp = spacy.load('en_core_web_lg')
+
+def preprocess(email : str) -> list:
+    vectors = []
+    doc = nlp(email)
+    vectors.append(doc.vector)
+    vector_df = pd.DataFrame(vectors) 
+    return vector_df
+
+model = load_model("99254.keras", )
+
+def email_classifier(email : str) -> str:
+    data = preprocess(email)
+    return 'safe' if model.predict(data) < 0.5 else 'not safe'
 
 app = Flask(__name__)
 
@@ -7,7 +27,7 @@ def index():
     if request.method == 'POST':
         mail = request.form['mail']
         print('ok')
-        return render_template('response.html', response = 'safe') # later email_classifier(mail)
+        return render_template('response.html', response = email_classifier(mail))
     else:
         return render_template('start.html')
 
